75a96d6ebde981f5250705a6157a82e54820fdcf
miniBear <ax_zaa@windowslive.com>
2025-06-20 04:38:16 +0700

    Fix: add bundle analyze

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -8,5 +9,14 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 4173,
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    process.env.ANALYZE === 'true' &&
+      visualizer({
+        filename: 'dist/stats.html',
+        open: true,
+        gzipSize: true,
+      }),
+  ].filter(Boolean),
 })
